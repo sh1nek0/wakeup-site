1a7659ac6f35e3fdd1ad9f13d3dd5803aa739f33
egoroz <egorsemirenko04@gmail.com>
2025-10-07 01:21:42 +0300

    back/

--- back/main.py
+++ back/main.py
@@ -1318,4 +1318,4 @@ def shutdown_scheduler():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=2)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=1)
